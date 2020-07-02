8779727726b9a0fc556f3e56c03db66f12ff42d0
Andrey208 <jukandrey208@gmail.com>
2020-07-02 14:56:07 +0300

    Remove 'Support' and 'Community' links from 'Help' tab

--- public/app/core/components/Footer/Footer.tsx
+++ public/app/core/components/Footer/Footer.tsx
@@ -17,18 +17,6 @@ export let getFooterLinks = (): FooterLink[] => {
       url: 'https://grafana.com/docs/grafana/latest/?utm_source=grafana_footer',
       target: '_blank',
     },
-    {
-      text: 'Support',
-      icon: 'question-circle',
-      url: 'https://grafana.com/products/enterprise/?utm_source=grafana_footer',
-      target: '_blank',
-    },
-    {
-      text: 'Community',
-      icon: 'comments-alt',
-      url: 'https://community.grafana.com/?utm_source=grafana_footer',
-      target: '_blank',
-    },
   ];
 };
 
