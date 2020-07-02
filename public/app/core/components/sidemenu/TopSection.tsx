00ca5be4a7292807d7d8ee79f3cada279f4b18ec
Andrey208 <jukandrey208@gmail.com>
2020-07-02 17:33:27 +0300

    Remove some nav item

--- public/app/core/components/sidemenu/TopSection.tsx
+++ public/app/core/components/sidemenu/TopSection.tsx
@@ -3,14 +3,27 @@ import _ from 'lodash';
 import TopSectionItem from './TopSectionItem';
 import config from '../../config';
 import { getLocationSrv } from '@grafana/runtime';
+import { contextSrv } from 'app/core/services/context_srv';
+
+interface NavItem {
+  id: string;
+}
 
 const TopSection: FC<any> = () => {
+  const excludeNavItemIds = ['create', 'dashboards', 'explore', 'alerting', 'admin'];
   const navTree = _.cloneDeep(config.bootData.navTree);
-  const mainLinks = _.filter(navTree, item => !item.hideFromMenu);
+  const mainLinks = _.filter(navTree, item => !item.hideFromMenu && !excludeNavItemIds.includes(item.id));
   const searchLink = {
     text: 'Search',
     icon: 'search',
   };
+
+  if (contextSrv.isSignedIn && contextSrv.user.isGrafanaAdmin === true) {
+    const cfg = _.find(mainLinks, x => x.id === 'cfg');
+
+    cfg.children = [];
+    cfg.url = '';
+  }
 
   const onOpenSearch = () => {
     getLocationSrv().update({ query: { search: 'open' }, partial: true });
